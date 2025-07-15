36cf041a7cd4057e2a94b1087d0ab1d42dff202e
Gaurav Prajapati <gauravprajapati111@outlook.com>
2025-07-15 18:43:02 +0530

    created sum program

--- input_demo.cpp
+++ input_demo.cpp
@@ -1,9 +1,14 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 
-int main() {
-    int x;
-    cin >> x;
-    cout << x*x;
+int main(){
+    int x,y;
+    freopen("input.txt","r", stdin);
+    freopen("output.txt","w", stdout);
+
+    cin >> x >> y;
+    cout <<"Sum : " << x + y <<endl;
     return 0;
-}
+
+
+} 
