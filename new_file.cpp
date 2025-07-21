46d570e00f39138f7a7a535e2faf494929e78f5c
Gaurav Prajapati <gauravprajapati111@outlook.com>
2025-07-21 21:34:09 +0530

    print value and name

---
+++ new_file.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    cout << "What is your name?\n";
+    cout << "My name is \"Gaurav Prajapati.\"\n";
+
+    cout << "What is your father's name?\n";
+    cout << "My father's name is \"Mr. Ramu Prajapati.\"\n";
+
+    cout << "Which college do you study?\n";
+    cout << "I'm studying in St.Andrew insitute of technology and Management.\n";
+
+    cout << 4 + 7 << "\n";
+
+    cout << 45 * 10 << "\n"; 
+
+    cout << 100 / 10 << "\n";
+    return 0;
+}
