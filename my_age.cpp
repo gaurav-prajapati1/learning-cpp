790944b396da9c537d4a9b456a85c641b69db081
Gaurav Prajapati <gauravprajapati111@outlook.com>
2025-07-22 20:45:35 +0530

    Using of data types

---
+++ my_age.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main() {
+    string name = "Gaurav";
+    int age = 19;
+    double height = 5.6;
+    cout << name << " is " << age << " yeras old and " << height << "feet tall.";
+    return 0;
+}
