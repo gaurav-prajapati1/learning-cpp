2aacdbb590cf77f126512da59b729a514e788ac3
Gaurav Prajapati <gauravprajapati111@outlook.com>
2025-07-27 23:01:03 +0530

    Calculate the area of square

---
+++ Square_area.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int side = 10;
+    int area = side * side;
+    cout << " Side of square: " << side << "\n";
+    cout << " Area of square is: " << area << "\n";
+    return 0;
+}
