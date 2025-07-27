aa989cc8abde3e1f3aa03c9aff6c9dbe2b8f9029
Gaurav Prajapati <gauravprajapati111@outlook.com>
2025-07-27 21:56:42 +0530

    Calculate the area of rectangle

---
+++ Rectangle_area.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+int length = 10;
+int width = 15;
+int area = length * width;
+cout << " Lenght is: " << length << "\n";
+cout << " Width is: " << width << "\n";
+cout << " Area of the rectangle is: " << area << "\n";
+return 0;
+
+}
--- multiple_variable.cpp
+++ multiple_variable.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main() {
-  int x = 7, y = 6, z = 35;;  
+  int x = 7, y = 6, z = 35;  
   cout << x + y + z;
   return 0;
 }
