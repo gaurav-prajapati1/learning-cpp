fb9b0da3ce5a5f85ce127480ab5b8f17b7fa46ff
Gaurav Prajapati <gauravprajapati111@outlook.com>
2025-07-30 21:32:55 +0530

    Print student details

---
+++ Student.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main() {
+ int studentId = 15;
+ int studentAge = 19;
+ float studentFee = 2000.400;
+  char studentGrade =  'A';
+ cout<< "Student ID: " << studentId << "\n";
+ cout<< "Student Age: " << studentAge << "\n";
+ cout<< "Student Fee: " << studentFee << "\n";
+ cout<< "Student Grade: " << studentGrade << "\n";
+ return 0;
+
+}
