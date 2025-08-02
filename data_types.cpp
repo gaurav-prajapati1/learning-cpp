02c619661e4640274fa90172c888a2a1e51d3146
Gaurav Prajapati <gauravprajapati111@outlook.com>
2025-08-02 21:37:21 +0530

    C++ Data Types

---
+++ data_types.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main () {
+    int myNum = 5;
+    float myFloatNum = 5.99;
+    double myDoubleNum = 9.99;
+    char myLetter = 'G';
+    bool myBoolean = true;
+    string myString = " Hello Gaurav Prajapati";
+    cout << "Int: "<< myNum << "\n";
+    cout<< "Float: "<< myFloatNum << "\n";
+    cout<< "Double: "<< myDoubleNum << "\n";
+    cout<< "Char: "<< myLetter << "\n";
+    cout<< "Bool: "<< myBoolean << "\n";
+    cout<< "String: "<< myString << "\n";
+    return 0;
+
+
+}
