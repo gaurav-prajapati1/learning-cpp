cfa7a223a530f809c2a2ded00e923d19d0bc70a0
Gaurav Prajapati <gauravprajapati111@outlook.com>
2025-08-09 22:19:57 +0530

    Concatenation

---
+++ concatination.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+    string firstName = "Gaurav";
+    string lastName = " Prajapati";
+    string fullName = firstName + lastName;
+cout << fullName ;
+return 0; 
+
+
+}
