76aba475a3920a2d1f1105c8363783ffb903db24
Gaurav Prajapati <gauravprajapati111@outlook.com>
2025-08-17 20:45:03 +0530

    Find the string lenght

---
+++ string-length.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+string text = "My name is Guarav Prajapati";
+cout << " The lenght of string: " << text.length();
+return 0;
+
+}
