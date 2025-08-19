3aac2891879e7f9f14bda91ccc5473431b914aaf
Gaurav Prajapati <gauravprajapati111@outlook.com>
2025-08-19 22:21:18 +0530

    Create a simple calculator

---
+++ simple_calculator.cpp
@@ -0,0 +1,13 @@
+#include <iostream> 
+using namespace std;
+int main(){
+    int x = 10;
+    int y = 5;
+ cout << "Addition = " << x + y << "\n";
+ cout << "Subtract = " << x - y<< "\n";
+ cout << "Multiply = " << x * y << "\n";
+ cout << "Divide = " << x / y << "\n";
+ return 0;
+
+
+}
