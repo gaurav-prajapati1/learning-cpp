829cebff35f96cafc316920687e80e0ba47dd7d9
Gaurav Prajapati <gauravprajapati111@outlook.com>
2025-08-19 21:02:28 +0530

    Add two integer

---
+++ Add_integer.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main (){
+    int x, y;
+    int sum;
+    cout << "a = ";
+    cin>> x;
+    cout << "b = ";
+    cin >> y;
+    sum = x + y;
+    cout<< sum;
+    return 0;
+}
