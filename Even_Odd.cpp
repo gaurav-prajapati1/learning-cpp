2444fe1c12d04e882ae8f88ac570f0b99794f78b
Gaurav Prajapati <gauravprajapati111@outlook.com>
2025-08-20 13:08:50 +0530

    Print whether number is even or odd.

---
+++ Even_Odd.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main (){
+    int Num;
+    cout << "Enter the Number ";
+    cin >> Num;
+    if ((Num%2)==0 )
+    cout << Num << " is Even";
+    else
+    cout << Num << " is Odd";
+    return 0;
+
+}
