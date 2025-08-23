8ad443671481f66c2453b3b74e53c37ea781bdbe
Gaurav Prajapati <gauravprajapati111@outlook.com>
2025-08-23 18:15:34 +0530

    sample for .gitignore

---
+++ diwakar.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int x , y;
+    int sum;
+    int multiply;
+    cout<< "Type a first number:";
+    cin >> x;
+    cout << "type a second number:";
+    cin >> y;
+    sum = x + y;
+    multiply = x * y;
+    cout << "Sum is:" << sum;
+
+    return 0;
+    
+
+} 
