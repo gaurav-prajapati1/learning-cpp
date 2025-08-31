be1a44027491daa941da6234ec40cb832e7372e9
Gaurav Prajapati <gauravprajapati111@outlook.com>
2025-08-31 23:18:11 +0530

    C++ program to evaluate prefix & postfix increment/decrement expressions

--- expressions.cpp
+++ expressions.cpp
@@ -2,103 +2,157 @@
 using namespace std;
 
 int main() {
-    int a = 10, b = 29, c = 6;
-  
+    int a, b, c;
 
-  int result1 = a++ + ++a;
+    // Expression 1
+    a = 10; b = 29; c = 6;
+    int result1 = a++ + ++a;
+    cout << "Result 1: " << result1 << endl;
 
-int result2 = --b + b++;
+    // Expression 2
+    a = 10; b = 29; c = 6;
+    int result2 = --b + b++;
+    cout << "Result 2: " << result2 << endl;
 
-int result3 = c-- - --c;
+    // Expression 3
+    a = 10; b = 29; c = 6;
+    int result3 = c-- - --c;
+    cout << "Result 3: " << result3 << endl;
 
-int result4 = ++a + --b - c++;
+    // Expression 4
+    a = 10; b = 29; c = 6;
+    int result4 = ++a + --b - c++;
+    cout << "Result 4: " << result4 << endl;
 
-int result5 = a++ - b-- + ++c;
+    // Expression 5
+    a = 10; b = 29; c = 6;
+    int result5 = a++ - b-- + ++c;
+    cout << "Result 5: " << result5 << endl;
 
-int result6 = --a + b++ + c--;
+    // Expression 6
+    a = 10; b = 29; c = 6;
+    int result6 = --a + b++ + c--;
+    cout << "Result 6: " << result6 << endl;
 
-int result7 = ++a - ++b + c++;
+    // Expression 7
+    a = 10; b = 29; c = 6;
+    int result7 = ++a - ++b + c++;
+    cout << "Result 7: " << result7 << endl;
 
-int result8 = a-- + --b - ++c;
+    // Expression 8
+    a = 10; b = 29; c = 6;
+    int result8 = a-- + --b - ++c;
+    cout << "Result 8: " << result8 << endl;
 
-int result9 = ++a + ++b + ++c;
+    // Expression 9
+    a = 10; b = 29; c = 6;
+    int result9 = ++a + ++b + ++c;
+    cout << "Result 9: " << result9 << endl;
 
-int result10 = a++ + b++ - c--;
+    // Expression 10
+    a = 10; b = 29; c = 6;
+    int result10 = a++ + b++ - c--;
+    cout << "Result 10: " << result10 << endl;
 
-int result11 = --a - b++ + c--;
+    // Expression 11
+    a = 10; b = 29; c = 6;
+    int result11 = --a - b++ + c--;
+    cout << "Result 11: " << result11 << endl;
 
-int result12 = a++ + --b + c++;
+    // Expression 12
+    a = 10; b = 29; c = 6;
+    int result12 = a++ + --b + c++;
+    cout << "Result 12: " << result12 << endl;
 
-int result13 = ++a - b-- - --c;
+    // Expression 13
+    a = 10; b = 29; c = 6;
+    int result13 = ++a - b-- - --c;
+    cout << "Result 13: " << result13 << endl;
 
-int result14 =  a-- - b++ + ++c;
+    // Expression 14
+    a = 10; b = 29; c = 6;
+    int result14 = a-- - b++ + ++c;
+    cout << "Result 14: " << result14 << endl;
 
-int result15 = ++a + b-- - c++;
+    // Expression 15
+    a = 10; b = 29; c = 6;
+    int result15 = ++a + b-- - c++;
+    cout << "Result 15: " << result15 << endl;
 
-int result16 = a++ - ++b + --c;
+    // Expression 16
+    a = 10; b = 29; c = 6;
+    int result16 = a++ - ++b + --c;
+    cout << "Result 16: " << result16 << endl;
 
-int result17 = --a + b++ + --c;
+    // Expression 17
+    a = 10; b = 29; c = 6;
+    int result17 = --a + b++ + --c;
+    cout << "Result 17: " << result17 << endl;
 
-int result18 = a++ + b-- + c++ - --a;
+    // Expression 18
+    a = 10; b = 29; c = 6;
+    int result18 = a++ + b-- + c++ - --a;
+    cout << "Result 18: " << result18 << endl;
 
-int result19 = ++a + --b - c++ + a--;
+    // Expression 19
+    a = 10; b = 29; c = 6;
+    int result19 = ++a + --b - c++ + a--;
+    cout << "Result 19: " << result19 << endl;
 
-int result20 = a++ - --b + c-- - ++c;
+    // Expression 20
+    a = 10; b = 29; c = 6;
+    int result20 = a++ - --b + c-- - ++c;
+    cout << "Result 20: " << result20 << endl;
 
-int result21 = ++a + b++ - --c + a--;
+    // Expression 21
+    a = 10; b = 29; c = 6;
+    int result21 = ++a + b++ - --c + a--;
+    cout << "Result 21: " << result21 << endl;
 
-int result22 = a-- + ++b + --c - b++;
+    // Expression 22
+    a = 10; b = 29; c = 6;
+    int result22 = a-- + ++b + --c - b++;
+    cout << "Result 22: " << result22 << endl;
 
-int result23 = --a + b-- - ++c + c++;
+    // Expression 23
+    a = 10; b = 29; c = 6;
+    int result23 = --a + b-- - ++c + c++;
+    cout << "Result 23: " << result23 << endl;
 
-int result24 = a++ + b++ + c++ - --a - --b;
+    // Expression 24
+    a = 10; b = 29; c = 6;
+    int result24 = a++ + b++ + c++ - --a - --b;
+    cout << "Result 24: " << result24 << endl;
 
-int result25 = ++a - --b + c-- + a++ - b--;
+    // Expression 25
+    a = 10; b = 29; c = 6;
+    int result25 = ++a - --b + c-- + a++ - b--;
+    cout << "Result 25: " << result25 << endl;
 
-int result26 = a-- - b-- + ++c + --a + b++;
+    // Expression 26
+    a = 10; b = 29; c = 6;
+    int result26 = a-- - b-- + ++c + --a + b++;
+    cout << "Result 26: " << result26 << endl;
 
-int result27 = ++a + --b + c++ - a-- + ++b;
+    // Expression 27
+    a = 10; b = 29; c = 6;
+    int result27 = ++a + --b + c++ - a-- + ++b;
+    cout << "Result 27: " << result27 << endl;
 
-int result28 = a++ - b++ + --c + ++a - c--;
+    // Expression 28
+    a = 10; b = 29; c = 6;
+    int result28 = a++ - b++ + --c + ++a - c--;
+    cout << "Result 28: " << result28 << endl;
 
-int result29 = --a + b++ - c-- + ++a - --b;
+    // Expression 29
+    a = 10; b = 29; c = 6;
+    int result29 = --a + b++ - c-- + ++a - --b;
+    cout << "Result 29: " << result29 << endl;
 
-int result30 = a++ + b-- + ++c - --a + c-- - ++b;
+    // Expression 30
+    a = 10; b = 29; c = 6;
+    int result30 = a++ + b-- + ++c - --a + c-- - ++b;
+    cout << "Result 30: " << result30 << endl;
 
-
-
-   
-
-
-cout << "Result 1 = "  << result1 << endl;
-cout << "Result 2 = "  << result2 << endl;
-cout << "Result 3 = "  << result3 << endl;
-cout << "Result 4 = "  << result4 << endl;
-cout << "Result 5 = "  << result5 << endl;
-cout << "Result 6 = "  << result6 << endl;
-cout << "Result 7 = "  << result7 << endl;
-cout << "Result 8 = "  << result8 << endl;
-cout << "Result 9 = "  << result9 << endl;
-cout << "Result 10 = " << result10 << endl;
-cout << "Result 11 = " << result11 << endl;
-cout << "Result 12 = " << result12 << endl;
-cout << "Result 13 = " << result13 << endl;
-cout << "Result 14 = " << result14 << endl;
-cout << "Result 15 = " << result15 << endl;
-cout << "Result 16 = " << result16 << endl;
-cout << "Result 17 = " << result17 << endl;
-cout << "Result 18 = " << result18 << endl;
-cout << "Result 19 = " << result19 << endl;
-cout << "Result 20 = " << result20 << endl;
-cout << "Result 21 = " << result21 << endl;
-cout << "Result 22 = " << result22 << endl;
-cout << "Result 23 = " << result23 << endl;
-cout << "Result 24 = " << result24 << endl;
-cout << "Result 25 = " << result25 << endl;
-cout << "Result 26 = " << result26 << endl;
-cout << "Result 27 = " << result27 << endl;
-cout << "Result 28 = " << result28 << endl;
-cout << "Result 29 = " << result29 << endl;
-cout << "Result 30 = " << result30 << endl;
- return 0;
+    return 0;
 }
