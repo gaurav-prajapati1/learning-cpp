00abd439a9cc943b256ec691380566f2ba2da127
Gaurav Prajapati <gauravprajapati111@outlook.com>
2026-07-07 17:18:23 +0530

    add function

--- tut10.cpp
+++ tut10.cpp
@@ -1,28 +1,60 @@
-#include <iostream>
-#include <string>
+// #include <iostream>
+// #include <string>
 
-using namespace std; 
+// using namespace std; 
+
+// int main() {
+   
+//     string name;      
+//     int magicNumber = 2;
+//     int userGuess;
+
+   
+//     cout << "Enter your name: "; 
+    
+//     getline(cin, name); 
+
+//     cout << "Hello, " << name << "! Guess the magic number (between 1 and 10): ";
+//     cin >> userGuess;
+
+    
+//     if (userGuess == magicNumber || userGuess == magicNumber) {
+//         cout << "Correct! You're a C++ master." << endl; 
+//     } else {
+//         cout << "Not quite. The magic number was " << magicNumber << "." << endl;
+//     }
+
+//     return 0;
+// }
+
+
+
+
+
+#include <iostream>
+using namespace std;
 
 int main() {
-   
-    string name;      
-    int magicNumber = 2;
-    int userGuess;
-
-   
-    cout << "Enter your name: "; 
+    int n;
     
-    getline(cin, name); 
+    cout << "Enter how many numbers: ";
+    cin >> n;
 
-    cout << "Hello, " << name << "! Guess the magic number (between 1 and 10): ";
-    cin >> userGuess;
+    int num, largest;
 
-    
-    if (userGuess == magicNumber || userGuess == magicNumber) {
-        cout << "Correct! You're a C++ master." << endl; 
-    } else {
-        cout << "Not quite. The magic number was " << magicNumber << "." << endl;
+    // Take first number
+    cin >> largest;
+
+    // Loop for remaining numbers
+    for(int i = 1; i < n; i++) {
+        cin >> num;
+
+        if(num > largest) {
+            largest = num;
+        }
     }
+
+    cout << "Largest number is: " << largest;
 
     return 0;
 }
